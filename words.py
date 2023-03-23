884ff96fbfe84b0601d6949bece48a7b03c6a5e0
hvingen <harold.van.ingen@isb.nl>
2023-03-23 18:46:52 +0100

    The Quick Python Book

--- words.py
+++ words.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 
+# Hiermee worden de woorden opgehaald en als lijst geretourneerd.
 def fetch_words():
 	with urlopen('http://sixty-north.com/c/t.txt') as story:
 		story_words = []
@@ -7,9 +8,18 @@ def fetch_words():
 			line_words = line.decode('utf-8').split()
 			for word in line_words:
 				story_words.append(word)
-		
+	return story_words
+
+
+# Hiermee wordt een lijst woorden afgedrukt.
+def print_words(story_words):
 	for word in story_words:
 		print(word)
-	
-if __name__=='__main__':
-	fetch_words()
+
+
+def main():
+	words = fetch_words()
+	print_words(words)
+
+if __name__=='__name_-':
+	main()
